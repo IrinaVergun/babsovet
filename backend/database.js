33125d2c2aea63ca4bb949a99a12b05b7747c683
Neli Harbuzava <neliharbuzavapublic@gmail.com>
2023-06-10 13:14:26 +0300

    Connect to Google sheets DB

---
+++ backend/database.js
@@ -0,0 +1,54 @@
+const { google } = require('googleapis');
+
+/*
+  Google Sheets as DB abstraction
+*/
+class Database {
+  constructor({ serviceAccountKeyFile, sheetId }) {
+    this.serviceAccountKeyFile = serviceAccountKeyFile;
+    this.sheetId = sheetId;
+  }
+
+  async _getClient() {
+    const auth = new google.auth.GoogleAuth({
+      keyFile: this.serviceAccountKeyFile,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+    const authClient = await auth.getClient();
+    return google.sheets({
+      version: 'v4',
+      auth: authClient,
+    });
+  }
+
+  async _readGoogleSheet(tabName, range) {
+    const googleSheetClient = await this._getClient();
+    const res = await googleSheetClient.spreadsheets.values.get({
+      spreadsheetId: this.sheetId,
+      range: `${tabName}!${range}`,
+    });
+
+    return res.data.values;
+  }
+
+  async _writeGoogleSheet(tabName, range, data) {
+    const googleSheetClient = await this._getClient();
+    await googleSheetClient.spreadsheets.values.append({
+      spreadsheetId: this.sheetId,
+      range: `${tabName}!${range}`,
+      valueInputOption: 'USER_ENTERED',
+      insertDataOption: 'INSERT_ROWS',
+      resource: {
+        "majorDimension": "ROWS",
+        "values": data
+      },
+    })
+  }
+
+  async getPasswords() {
+    const data = await this._readGoogleSheet('Passwords', 'A:B');
+    console.log(data);
+  }
+}
+
+module.exports = Database;
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
 require('dotenv').config()
 const path = require("path");
-const express = require('express')
+const express = require('express');
+const Database = require('./database');
 const app = express()
 const port = process.env.PORT || 3000;
+
+const DBOptions = {
+  serviceAccountKeyFile: process.env.GOOGLE_SERVICE_ACCOUNT_KEY_FILE_PATH,
+  sheetId: process.env.GOOGLE_SHEET_ID
+};
+
+const DB = new Database(DBOptions)
 
 // React web app
 app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 app.use(express.static("public"));
 
-app.get('/hello', (req, res) => {
+app.get('/hello', async (req, res) => {
+  await DB.getPasswords();
   res.send('Hello World!')
 })
 
