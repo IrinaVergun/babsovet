858ead068d6e068a3eaa6c936568b85c10c29ccc
Neli Harbuzava <neliharbuzavapublic@gmail.com>
2023-06-10 12:21:00 +0300

    Server set up

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
+const path = require("path");
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+// React web app
+app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+app.use(express.static("public"));
+
+app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
 
