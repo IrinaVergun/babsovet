6dddca9e511558c863cc545de6628d9b13501cd1
Irina Vergun <antohka803@gmail.com>
2023-06-20 23:26:08 +0300

    backend - enable cors

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,46 @@
-require('dotenv').config()
+require("dotenv").config();
 const path = require("path");
-const express = require('express');
-const Database = require('./database');
-const { generateAccessToken } = require('./autorization');
-const bodyParser = require('body-parser')
+const express = require("express");
+const Database = require("./database");
+const { generateAccessToken } = require("./autorization");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express()
+app.use(cors());
+
+const app = express();
 const port = process.env.PORT || 3000;
 
 // JSON
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 const DBOptions = {
   serviceAccountKeyFile: process.env.GOOGLE_SERVICE_ACCOUNT_KEY_FILE_PATH,
-  sheetId: process.env.GOOGLE_SHEET_ID
+  sheetId: process.env.GOOGLE_SHEET_ID,
 };
 
-const DB = new Database(DBOptions)
+const DB = new Database(DBOptions);
 
 // React web app
 app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 app.use(express.static("public"));
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const users = await DB.getUsers();
-  const user = users.find(user => user.password === req.body.password);
+  const user = users.find((user) => user.password === req.body.password);
 
   if (!user) {
     return res.status(401).json({
-      error: 'Нет такого чела'
+      error: "Нет такого чела",
     });
   }
 
   const token = generateAccessToken({ username: req.body.username });
   return res.status(200).json({
-    token
+    token,
   });
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
